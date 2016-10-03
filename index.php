59eda0e74fa73093f864da93ee0d71eaaa7b3a48
1308830 <dennis1_obrien@hotmail.com>
2016-10-03 16:12:57 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+/**
+ * Created by PhpStorm.
+ * User: 1308830
+ * Date: 03/10/2016
+ * Time: 15:50
+ */
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<p>
+<?php
+echo "Hello World";
+echo 5 * 7;
+?>
+</p>
+</body>
+</html>
