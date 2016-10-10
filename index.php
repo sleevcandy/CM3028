0751a0a437e35d239424c0e2591d6bcd782b14b0
1308830 <dennis1_obrien@hotmail.com>
2016-10-10 16:04:43 +0100

    come mitts

--- index.php
+++ index.php
@@ -6,18 +6,36 @@
  */
 <!DOCTYPE html>
 <html>
-<head>
-</head>
+    <head>
+    $databaseName = "acsm_e63985346002dbb";
+    </head>
 <body>
 <p>
 <?php
-$name = "Edgar";
-if($name == "Simon"){
-    print "i know you!";
+// connect to csdm webdev server and select database
+$db = new mysqli(
+    "hostname",
+    "username",
+    "password",
+    "db_name"
+);
+// tesyt if connection was established, and print any errors
+if($db->connection_errno){
+    die('Connect failed['.$db->connect_error.']');
 }
-else{
-    print "Who are youhoo?";
+// create a SQL query as a string
+$sql_query = "SELECT * FROM superheroes WHERE superpower LIKE '% aser %'";
+//execute the SQL query
+$result = $db->fetch_array()){
+    // process the $row
 }
+while($row = $result->fetch_array()){
+    // print out fields from row of daya
+    echo"<p>".$row['superheroName']."</p>";
+}
+$result->dose();
+// dose connection to database
+$db->dose();
 ?>
 </p>
 </body>
