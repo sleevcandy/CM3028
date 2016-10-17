137e9a8252fedaacb75dfa704e303a373eba87b5
1308830 <dennis1_obrien@hotmail.com>
2016-10-17 15:15:21 +0100

    come mitts

---
+++ week 2/index1.php
@@ -0,0 +1,22 @@
+
+/**
+ * Created by PhpStorm.
+ * User: 1308830
+ * Date: 17/10/2016
+ * Time: 15:10
+ */
+
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        <p>
+            <?php
+                echo "back to the start aye";
+            ?>
+            </p>
+    </body>
+</html>
+
+
